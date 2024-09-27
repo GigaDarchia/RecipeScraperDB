2fdf7974592243304782dde227b6c436c3e315ab
Giga Darchia <sebagedarchia@gmail.com>
2024-09-27 23:02:06 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from database import RecipeDatabase
 from scrapper import Scrapper
 import asyncio
 
-
 async def main():
     scrapper = Scrapper()
     result_data = await scrapper.scrap()
@@ -10,6 +9,14 @@ async def main():
 
     db.populate_collection(result_data)
 
+    db.get_average_ingredients()
+    db.get_average_steps()
+    db.get_recipe_with_most_portions()
+    db.fetch_author_with_most_recipes()
+
+    db.close()
+
 
 if __name__ == "__main__":
     asyncio.run(main())
+
