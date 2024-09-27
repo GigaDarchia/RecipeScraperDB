de6c7856722ccd20d883b65743dc3ea523c2bc72
lmamu21 <lmamu21@freeuni.edu.ge>
2024-09-27 22:49:40 +0400

    implemented asynchronious programming for fetching data

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from database import RecipeDatabase
 from scrapper import Scrapper
+import asyncio
 
-db = RecipeDatabase("recipes", "recipes")
-scrapper = Scrapper()
-result_data = scrapper.scrap()
-db.populate_collection(result_data)
+
+async def main():
+    scrapper = Scrapper()
+    result_data = await scrapper.scrap()
+    db = RecipeDatabase("recipes", "recipes")
+
+    db.populate_collection(result_data)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- scrapper.py
+++ scrapper.py
@@ -1,7 +1,7 @@
-import json
-
 from bs4 import BeautifulSoup
-import requests
+import asyncio
+import aiohttp
+import threading
 
 
 def get_amount(s):
@@ -12,6 +12,11 @@ def get_amount(s):
         return 1
 
 
+async def fetch(url, session):
+    async with session.get(url) as response:
+        return await response.text()
+
+
 class Scrapper:
     def __init__(self):
         self.main_containers = None
@@ -20,46 +25,49 @@ class Scrapper:
         self.recipes = f"{self.base_url}/receptebi"
         self.categories = f"{self.recipes}/cat"
         self.target_category = f"{self.categories}/pasta-da-burRuleuli"
+        self.result_lock = threading.Lock()
 
-    def scrap(self):
-        main_page = requests.get(self.target_category)
-        self.soup = BeautifulSoup(main_page.text, "html.parser")
-        self.main_containers = []
-        first_container = self.soup.find(attrs={"class": "section"}).find(attrs={"class": "box-container"})
+    async def scrap(self):
+        async with aiohttp.ClientSession() as session:
+            main_page = await fetch(self.target_category, session)
+            self.soup = BeautifulSoup(main_page, "html.parser")
+            self.main_containers = []
+            first_container = self.soup.find(attrs={"class": "section"}).find(attrs={"class": "box-container"})
+            self.main_containers.append(first_container)
 
-        self.main_containers.append(first_container)
+            result = []
+            await self.scrap_pages(result, session)
+            return result
 
-        result = []
-
-        self.scrap_pages(result)
-
-        return result
-
-    def scrap_pages(self, result):
+    async def scrap_pages(self, result, session):
         while len(self.main_containers) > 0:
             container = self.main_containers.pop(0)
             items = container.find_all("div", recursive=False)
 
+            tasks = []
             for item in items:
                 if "endless_container" in item["class"]:
                     next_page_uri = item.find("a")["href"]
                     next_page_url = f"{self.base_url}{next_page_uri}"
-                    next_page = requests.get(next_page_url)
-                    soup = BeautifulSoup(next_page.text, "html.parser")
-                    container = soup.find(attrs={"class": "section"}).find(attrs={"class": "box-container"})
-                    self.main_containers.append(container)
-
+                    tasks.append(self.handle_next_page(next_page_url, session))
                 else:
-                    self.scrap_item(item, result)
+                    tasks.append(self.scrap_item(item, result, session))
 
-    def scrap_item(self, item, result):
+            await asyncio.gather(*tasks)
+
+    async def handle_next_page(self, next_page_url, session):
+        next_page = await fetch(next_page_url, session)
+        soup = BeautifulSoup(next_page, "html.parser")
+        container = soup.find(attrs={"class": "section"}).find(attrs={"class": "box-container"})
+        self.main_containers.append(container)
+
+    async def scrap_item(self, item, result, session):
         recipe = {}
         img_box = item.find("div", attrs={"class": "box__img"})
         box_space = item.find("div", attrs={"class": "box-space"})
 
-        url_and_title = box_space.find("a")
-
         # 1. title of recipe
+        url_and_title = box_space.find("a")
         title = url_and_title.contents[0].strip()
         recipe["title"] = title
 
@@ -69,8 +77,7 @@ class Scrapper:
 
         # 3. main image url
         image_uri = img_box.find("img")["src"]
-        image_url = f"{self.base_url}{image_uri}"
-        recipe["image_url"] = image_url
+        recipe["image_url"] = f"{self.base_url}{image_uri}"
 
         # 4. description
         short_description = box_space.find("div", attrs={"class": "box__desc"}).contents[0].strip()
@@ -80,9 +87,9 @@ class Scrapper:
         author = box_space.find("div", attrs={"class": "name"}).find("a").contents[0].strip()
         recipe["author"] = author
 
-        # now scrapping recipe page itself
-        recipe_page = requests.get(recipe["url"])
-        soup = BeautifulSoup(recipe_page.text, "html.parser")
+        # Now scrap the recipe page itself
+        recipe_page = await fetch(recipe["url"], session)
+        soup = BeautifulSoup(recipe_page, "html.parser")
 
         # 6. number of portions
         portion_container = soup.find_all("div", attrs={"class": "lineDesc__item"})[1]
@@ -120,10 +127,11 @@ class Scrapper:
         subcategory_uri = categories[3]["href"]
         subcategory_url = f"{self.base_url}{subcategory_uri}"
 
-        category = {"name": category_name, "url": category_url}
-        subcategory = {"name": subcategory_name, "url": subcategory_url}
+        recipe["category"] = {"name": category_name, "url": category_url}
+        recipe["subcategory"] = {"name": subcategory_name, "url": subcategory_url}
 
-        recipe["category"] = category
-        recipe["subcategory"] = subcategory
-
+        self.result_lock.acquire()
         result.append(recipe)
+        self.result_lock.release()
+
+
